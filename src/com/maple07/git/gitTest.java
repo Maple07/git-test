01539336923e1d4681c3ae98966f9773fc42b853
husky <349166674@qq.com>
2022-01-25 21:06:42 +0800

    second commit

--- src/com/maple07/git/gitTest.java
+++ src/com/maple07/git/gitTest.java
@@ -3,6 +3,7 @@ package com.maple07.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
